f73623bb0b8be25b08021fb9f4768a9c711a2a3b
Reha Sterbin <reha@omniti.com>
2020-06-29 14:04:37 -0400

    Need this for a test, will remove later

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,6 +4,9 @@ var corsHeaders = function(req, res, next) {
     if (!req.originalUrl.match('^/api')) {
         return next();
     }
+    if (req.originalUrl.match('^/test')) {
+        console.log(JSON.stringify(req.headers));
+    }
 
     const prod_origin = 'https://hih-covid-benefits-back.herokuapp.com';
     const dev_origins = [ 'http://localhost:3000', 'http://localhost:3001', 'http://localhost2:3001' ];
