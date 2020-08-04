4d4996c79dc5ea550f6458a8db8d0c02177d52f8
Reha Sterbin <reha.sterbin@credativ.us>
2020-08-04 01:58:21 -0400

    Forgot admin requires an english translation

--- sql/fixes/disclaimer.sql
+++ sql/fixes/disclaimer.sql
@@ -3,5 +3,16 @@
 --
 
 INSERT INTO language_keys (key, section, help, markdown_allowed) VALUES
-('disclaimer_header', 'disclaimer', 'The header for the disclaimer', FALSE),
-('disclaimer_text', 'disclaimer', 'The disclaimer text', TRUE);
+    ('disclaimer_header', 'disclaimer', 'The header for the disclaimer', FALSE),
+    ('disclaimer_text', 'disclaimer', 'The disclaimer text', TRUE);
+
+INSERT INTO translations (key_id, language, translation)
+    SELECT key_id, 'en', 'PLACEHOLDER'
+    FROM language_keys
+    WHERE key = 'disclaimer_header';
+
+INSERT INTO translations (key_id, language, translation)
+    SELECT key_id, 'en', 'PLACEHOLDER'
+    FROM language_keys
+    WHERE key = 'disclaimer_text';
+
