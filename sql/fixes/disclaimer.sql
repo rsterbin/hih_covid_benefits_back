1ee065a74b57354935616e5cb2ae6b83c58cb23d
Reha Sterbin <reha.sterbin@credativ.us>
2020-08-04 10:08:13 -0400

    Link language

--- sql/fixes/disclaimer.sql
+++ sql/fixes/disclaimer.sql
@@ -4,7 +4,10 @@
 
 INSERT INTO language_keys (key, section, help, markdown_allowed) VALUES
     ('disclaimer_header', 'disclaimer', 'The header for the disclaimer', FALSE),
-    ('disclaimer_text', 'disclaimer', 'The disclaimer text', TRUE);
+    ('disclaimer_text', 'disclaimer', 'The disclaimer text', TRUE),
+    ('util_back_to_quiz', 'utilities', 'The text for a link that goes back to the quiz', FALSE),
+    ('landing_disclaimer_link', 'landing', 'The text for the link to the disclaimer', FALSE),
+    ('landing_disclaimer_title', 'landing', 'The title text for the disclaimer link', FALSE);
 
 INSERT INTO translations (key_id, language, translation)
     SELECT key_id, 'en', 'PLACEHOLDER'
@@ -15,4 +18,19 @@ INSERT INTO translations (key_id, language, translation)
     SELECT key_id, 'en', 'PLACEHOLDER'
     FROM language_keys
     WHERE key = 'disclaimer_text';
+
+INSERT INTO translations (key_id, language, translation)
+    SELECT key_id, 'en', 'PLACEHOLDER'
+    FROM language_keys
+    WHERE key = 'util_back_to_quiz';
+
+INSERT INTO translations (key_id, language, translation)
+    SELECT key_id, 'en', 'PLACEHOLDER'
+    FROM language_keys
+    WHERE key = 'landing_disclaimer_link';
+
+INSERT INTO translations (key_id, language, translation)
+    SELECT key_id, 'en', 'PLACEHOLDER'
+    FROM language_keys
+    WHERE key = 'landing_disclaimer_title';
 
