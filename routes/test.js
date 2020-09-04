8f46eefa4869edce4a8d2c64876c8fa119afa8fa
Reha Sterbin <reha.sterbin@credativ.us>
2020-09-04 17:19:39 -0400

    Testing bounce

--- routes/test.js
+++ routes/test.js
@@ -4,12 +4,18 @@ var router = express.Router();
 /* TODO: Event bounce: scrapers get social media headers, real people get forwarded to the mobilize site */
 router.get('/event/:code', function(req, res, next) {
     console.log('user agent on event bounce is: ', req.get('User-Agent'));
-    res.render('test-event', {
-        title: 'This is a test',
-        description: 'This is a description',
-        image: 'https://www.nydomesticworkbenefits.org/images/test.jpg',
-        link: 'http://www.example.com/'
-    });
+    const ua = req.get('User-Agent');
+    const fw = 'http://www.example.com/';
+    if (ua.match('facebook')) {
+        res.render('event', {
+            title: 'This is a test',
+            description: 'This is a description',
+            image: 'https://www.nydomesticworkbenefits.org/images/test.jpg',
+            fw_link: fw
+        });
+    } else {
+        res.redirect(302, fw);
+    }
 });
 
 module.exports = router;
