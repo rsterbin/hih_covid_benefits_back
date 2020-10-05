c847f3a2472464e0ddacd7c3b37b5a94486cf5fa
Reha Sterbin <reha.sterbin@credativ.us>
2020-10-05 19:22:07 -0400

    Up to speed with redux admin front end changes

--- routes/api/v1.0/admin/contacts.js
+++ routes/api/v1.0/admin/contacts.js
@@ -32,4 +32,30 @@ router.post('/recent', async function(req, res, next) {
     }
 });
 
+// admin/contacts/raw POST: get all the raw contacts
+router.post('/raw', async function(req, res, next) {
+    if (typeof(req.body) !== 'object') {
+        res.status(400);
+        res.json({ code: 'NO_DATA', msg: 'No data was provided' });
+        return;
+    }
+    if (!req.body.token) {
+        res.status(403);
+        res.json({ code: 'TOKEN_REQUIRED', msg: 'Token is required' });
+        return;
+    }
+    const check = await sessionLogic.checkToken(req.body.token);
+    if (!check.ok) {
+        res.status(check.data.status);
+        res.json({ code: check.data.code, msg: 'Invalid session' });
+    }
+    const all = await contactLogic.getRaw();
+    if (!all.ok) {
+        res.status(all.data.status);
+        res.json({ code: all.data.code, msg: 'Could not get recent contacts' });
+    } else {
+        res.json({ msg: 'Fetched', contacts: all.data.all });
+    }
+});
+
 module.exports = router;
