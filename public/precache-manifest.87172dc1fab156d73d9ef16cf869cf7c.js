408896ecc618921830f10126f1e1b4181cefaff3
Reha Sterbin <reha.sterbin@credativ.us>
2021-03-30 15:00:22 -0400

    Front end changes

---
+++ public/precache-manifest.87172dc1fab156d73d9ef16cf869cf7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a4e4cb1a359750276cbe6359b5122a",
+    "url": "https://www.nydomesticworkbenefits.org/index.html"
+  },
+  {
+    "revision": "ef919bdcb34fe2739913",
+    "url": "https://www.nydomesticworkbenefits.org/static/css/2.9de2e04a.chunk.css"
+  },
+  {
+    "revision": "d8b23b31b7de2c72db2b",
+    "url": "https://www.nydomesticworkbenefits.org/static/css/main.8c6b0cb0.chunk.css"
+  },
+  {
+    "revision": "ef919bdcb34fe2739913",
+    "url": "https://www.nydomesticworkbenefits.org/static/js/2.ab369b0c.chunk.js"
+  },
+  {
+    "revision": "4c46a5f0c8b75dfeef04f7cb0aab9c10",
+    "url": "https://www.nydomesticworkbenefits.org/static/js/2.ab369b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b23b31b7de2c72db2b",
+    "url": "https://www.nydomesticworkbenefits.org/static/js/main.b81c45ef.chunk.js"
+  },
+  {
+    "revision": "40cf7744fe9078c7ec78",
+    "url": "https://www.nydomesticworkbenefits.org/static/js/runtime-main.8d71be8c.js"
+  }
+]);
